b6c8a5a016df5ede6b6063b66f5219d354c5c146
Lynda Santiago <you@example.com>
2020-08-14 00:55:05 -0400

    successfully converted application into using Context API

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import {UserContext} from './contexts/ProductContext'
-
+import {CartContext} from './contexts/CartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -21,7 +21,7 @@ function App() {
 	return (
 		<div className="App">
 			<UserContext.Provider value={{products, addItem}}>
-			<Navigation cart={cart} />
+			<CartContext.Provider value={{cart}}><Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
@@ -31,6 +31,7 @@ function App() {
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</CartContext.Provider>
 			</UserContext.Provider>
 		</div>
 	);
